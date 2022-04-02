4338afd5b208a3f164e3e2a27756e6f5fd686425
Jason Ancrum <jason.ancrum00@delta.com>
2022-04-02 10:55:18 -0400

    initial push of revel app

---
+++ myapp/app/controllers/app.go
@@ -0,0 +1,25 @@
+package controllers
+
+import (
+	"github.com/revel/revel"
+)
+
+type App struct {
+	*revel.Controller
+}
+
+func (c App) Index() revel.Result {
+	return c.Render()
+}
+
+func (c App) Hello(myName string) revel.Result {
+	c.Validation.Required(myName).Message("The name here is required")
+	c.Validation.MinSize(myName, 3).Message("The name given is not long enough")
+
+	if c.Validation.HasErrors() {
+		c.Validation.Keep()
+		c.FlashParams()
+		return c.Redirect(App.Index)
+	}
+	return c.Render(myName)
+}
